806af9c74e2a9348984ad0035b9897f26a94faeb
Phantom <144842571+Phantomasia@users.noreply.github>
2023-12-14 00:39:18 +0300

    Added another solution for the palindrome problem

--- palindrome.ts
+++ palindrome.ts
@@ -1,4 +1,4 @@
-function isPalindrome(x: number): boolean {
+function isPalindromeString(x: number): boolean {
   let x_to_String = x.toString();
   let reversed_string = x_to_String.split("").reverse().join("");
   if (x_to_String === reversed_string) {
@@ -8,7 +8,27 @@ function isPalindrome(x: number): boolean {
   }
 }
 
-const trueCase = isPalindrome(565);
-const falseCase = isPalindrome(766);
+function isPalindromeNumber(x: number): boolean {
+  if (x < 0) return false;
+  let reversed_sum = 0;
+  let original_x = x;
+  while (x > 0) {
+    reversed_sum = reversed_sum * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  if (original_x === reversed_sum) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-console.log(trueCase, falseCase);
+const trueCase1 = isPalindromeString(565);
+const falseCase1 = isPalindromeString(766);
+
+const trueCase2 = isPalindromeNumber(808);
+const falseCase2 = isPalindromeNumber(879);
+console.log({
+  palindrom_using_string: `the first case using String is **${trueCase1}** while the second is **${falseCase1}**`,
+  palindrome_using_number: `the first case using Number is **${trueCase2}** while the second is **${falseCase2}**`,
+});
